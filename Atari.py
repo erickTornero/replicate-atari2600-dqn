db9789decf8fa740cb3ee0bf7b37af5ca15689d4
erickTorneroT <erickdeivy01@gmail.com>
2019-01-19 05:50:21 -0500

    break-out model loaded

---
+++ Atari.py
@@ -0,0 +1,59 @@
+from ale_python_interface import ALEInterface
+import numpy as np 
+import cv2
+from random import randrange
+import os
+import sys
+
+class Atari:
+	def __init__(self,rom_name):
+		self.ale = ALEInterface()
+		self.max_frames_per_episode = self.ale.getInt(b"max_num_frames_per_episode")
+		self.ale.setInt(b"random_seed",123)
+		self.ale.setInt(b"frame_skip",4)
+
+		USE_SDL = True
+		if USE_SDL:
+		  if sys.platform == 'darwin':
+		    import pygame
+		    pygame.init()
+		    self.ale.setBool(b'sound', False) # Sound doesn't work on OSX
+		  elif sys.platform.startswith('linux'):
+		    self.ale.setBool(b'sound', True)
+		self.ale.setBool(b'display_screen', True)
+
+		# Define direction of ROM
+		#print( 'cwd: ', os.getcwd() )
+		dirROM = os.path.join( os.getcwd(), "roms/breakout.bin" )
+		#print( 'dirROM: ', dirROM )
+		rom_file = str.encode(dirROM)
+		self.ale.loadROM(rom_file)
+		self.screen_width,self.screen_height = self.ale.getScreenDims()
+		self.legal_actions = self.ale.getMinimalActionSet()
+		self.action_map = dict()
+		for i in range(len(self.legal_actions)):
+			self.action_map[self.legal_actions[i]] = i
+		#print len(self.legal_actions)
+		self.windowname = rom_name
+		cv2.startWindowThread()
+		cv2.namedWindow(rom_name)
+
+	def get_image(self):
+		numpy_surface = np.zeros(self.screen_height*self.screen_width*3, dtype=np.uint8)
+		self.ale.getScreenRGB(numpy_surface)
+		image = np.reshape(numpy_surface, (self.screen_height, self.screen_width, 3))
+		return image
+
+	def newGame(self):
+		self.ale.reset_game()
+		return self.get_image()
+
+	def next(self, action):
+		reward = self.ale.act(self.legal_actions[np.argmax(action)])	
+		nextstate = self.get_image()
+		
+		cv2.imshow(self.windowname,nextstate)
+		if self.ale.game_over():
+			self.newGame()
+		#print "reward %d" % reward 
+		return nextstate, reward, self.ale.game_over()
---
+++ AtariDQN.py
@@ -0,0 +1,85 @@
+import cv2
+import sys
+sys.path.append("game/")
+from Atari import Atari 
+from BrainDQN_Nature import *
+import numpy as np 
+
+TEST = True
+
+# preprocess raw image to 80*80 gray image
+def preprocess(observation):
+	observation = cv2.cvtColor(cv2.resize(observation, (84, 110)), cv2.COLOR_BGR2GRAY)
+	observation = observation[26:110,:]
+	ret, observation = cv2.threshold(observation,1,255,cv2.THRESH_BINARY)
+	return np.reshape(observation,(84,84,1))
+
+def playAtari():
+	# Step 1: init BrainDQN
+	# Step 2: init Flappy Bird Game
+	atari = Atari('breakout.bin')
+	actions = len(atari.legal_actions)
+	brain = BrainDQN(actions)
+	
+	
+	# Step 3: play game
+	# Step 3.1: obtain init state
+	action0 = np.array([1,0,0,0])  # do nothing
+	observation0, reward0, terminal = atari.next(action0)
+	observation0 = cv2.cvtColor(cv2.resize(observation0, (84, 110)), cv2.COLOR_BGR2GRAY)
+	observation0 = observation0[26:110,:]
+	ret, observation0 = cv2.threshold(observation0,1,255,cv2.THRESH_BINARY)
+	brain.setInitState(observation0)
+	print(atari.ale.getLegalActionSet())
+	limitsameactions = 1000
+	action_prev = 0
+	counter_actionssame = 0
+	# Step 3.2: run the game
+	if not TEST:
+		while 1!= 0:
+			if TEST :
+				action = brain.getActionTest()
+			else:
+				action = brain.getAction()
+			nextObservation,reward,terminal = atari.next(action)
+			nextObservation = preprocess(nextObservation)
+			brain.setPerception(nextObservation,action,reward,terminal)
+	else:
+		while True:
+			total_reward = 0
+			while not atari.ale.game_over():
+				action = brain.getActionTest()
+				nextObservation,reward,terminal = atari.next(action)
+				nextObservation = preprocess(nextObservation)
+				brain.setPerceptionTest(nextObservation,action,reward,terminal)
+				total_reward += reward
+				#print(counter_actionssame)
+				action_curidx = getActionIdx(action)
+				if action_prev == action_curidx:
+					counter_actionssame = counter_actionssame + 1
+					if counter_actionssame == 100:
+						atari.ale.reset_game()
+				else:
+					counter_actionssame = 0
+				action_prev = action_curidx
+				counter_actionssame = counter_actionssame + 1
+				#if(counter_actionssame % 100 == 0):
+				#if(not terminal):
+				#	print('terminal')
+				#	atari.ale.reset_game()
+				#print(atari.ale.game_over())
+			print('Episode Ended with score: %d' %(total_reward))
+			atari.ale.reset_game()
+			
+def getActionIdx(arr):
+	i = 0
+	for i in range(len(arr)):
+		if(arr[i] > 0):
+			return i
+	return i
+
+def main():
+	playAtari()
+
+if __name__ == '__main__':
+	main()
